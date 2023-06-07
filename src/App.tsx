7d2afad096181f55cae7c7495090fe9cc8a99f9d
gayanDeeegala <gayandeegala@gmail.com>
2023-06-07 22:09:40 +0530

    theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { ConfigProvider, theme, Button } from 'antd';
+import { styled } from 'styled-components';
 
 function App() {
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  useEffect(() => {
+    setIsDarkMode(true);
+  }, []);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Background>
+      <ConfigProvider
+        theme={{
+          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+          token: {
+            colorPrimary: '#B30000',
+          },
+        }}
+      >
+        <Button type='primary'>Change Theme to {isDarkMode ? 'Light' : 'Dark'}</Button>
+      </ConfigProvider>
+    </Background>
   );
 }
 
 export default App;
+
+const Background = styled.div`
+  background-color: rgba(0, 0, 0, 0.88);
+  min-height: 100vh;
+`;
---
+++ src/less-vars-to-js.d.ts
@@ -0,0 +1,4 @@
+declare module 'less-vars-to-js' {
+  function lessVarsToJs(input: string): Record<string, string>;
+  export default lessVarsToJs;
+}
